15a6e00666f03be5a6b16041bf7560e81d0a970c
MIYA <miya2yummy@gmail.com>
2017-06-19 16:53:03 +0800

    update viewpager tab bar

--- app/src/main/java/edu/np/ece/attendancetakingapplication/DetailsActivity.java
+++ app/src/main/java/edu/np/ece/attendancetakingapplication/DetailsActivity.java
@@ -4,22 +4,30 @@ package edu.np.ece.attendancetakingapplication;
 
 //import android.app.FragmentManager;
 //import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.net.Uri;
+import android.support.annotation.ColorRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.j256.ormlite.stmt.query.In;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.Bundler;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,23 +55,29 @@ public  class DetailsActivity extends FragmentActivity  {
     ViewPager viewPager;
     List<Fragment> fragmentList;
     FragmentManager manager;
+    private FragmentPagerItemAdapter adapter;
+    private PagerTabStrip pagerTabStrip;
+    private String[] tabNames;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         viewPager=(ViewPager)this.findViewById(R.id.viewPage);
+        pagerTabStrip=(PagerTabStrip)this.findViewById(R.id.viewpagertab);
+        pagerTabStrip.setDrawFullUnderline(true);
+        pagerTabStrip.setTextSize(TypedValue.COMPLEX_UNIT_SP,20);
+
         fragmentList = new ArrayList<Fragment>();
 
 
+        tabNames=getResources().getStringArray(R.array.tabs);
         Intent intent = getIntent();
         if(intent!=null){
 
 
-           // android.app.Fragment fragment = null;
             Fragment fragment = new LessonDetailsFragment();
-           // FragmentManager fragmentManager = getFragmentManager();
-            //FragmentTransaction transaction= fragmentManager.beginTransaction();
              manager = getSupportFragmentManager();
             FragmentTransaction transaction= manager.beginTransaction();
 
@@ -87,9 +101,57 @@ public  class DetailsActivity extends FragmentActivity  {
             ViewpagerAdapter viewpagerAdapter = new ViewpagerAdapter(getSupportFragmentManager(),fragmentList);
             viewPager.setAdapter(viewpagerAdapter);
 
+
+
+
+
+            /**
+            FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                    getSupportFragmentManager(), FragmentPagerItems.with(this)
+                    .add(R.string.tab_details_title, LessonDetailsFragment.class)
+                    .add(R.string.tab_history_title, HistoryByLessonFragment.class)
+                    .create());
+
+            viewPager.setAdapter(adapter);
+
+            SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+            viewPagerTab.setCustomTabView(R.string.tab_details_title,R.id.viewpagertab);
+            viewPagerTab.setCustomTabView(R.string.tab_history_title,R.id.viewpagertab);
+
+            viewPagerTab.setViewPager(viewPager);*/
+
+
+
         }
 
         
+    }
+    public class ViewpagerAdapter extends FragmentPagerAdapter {
+        List<Fragment> fragmentList = new ArrayList<Fragment>();
+        public ViewpagerAdapter(FragmentManager fm,List<Fragment> fragments) {
+            super(fm);
+            fragmentList=fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            return fragmentList.get(position);
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            return tabNames[position];
+        }
+
+        @Override
+        public int getCount() {
+            //   return fragmentList!=null?fragmentList.size():0;
+            return fragmentList.size();
+        }
+
     }
 
 
@@ -98,5 +160,6 @@ public  class DetailsActivity extends FragmentActivity  {
 
 
 
-
 }
+
+
